7b0c243dda8c0eb0ff154d9a4503e57392f2717b
kushuu <sonikushu007@gmail.com>
2022-03-24 01:03:47 +0530

    added more plots📈

--- app.py
+++ app.py
@@ -28,29 +28,28 @@ layout = dict(
 
 
 wineries = st.sidebar.multiselect('Show wineries.', df['winery'].unique())
-prices = st.sidebar.multiselect('Show prices.', df['price'].unique())
-rating = st.sidebar.multiselect('Show rating.', df['rating'].unique())
 category = st.sidebar.multiselect('Show category.', df['category'].unique())
 
 new_df = df[df['winery'].isin(wineries)]
 new_df.reset_index(drop = True, inplace = True)
 st.write(new_df)
+top_wineries = new_df.winery.value_counts()
+tt = new_df.winery.value_counts().dropna()
+winery_price = new_df[(new_df.price >= 14) & (new_df.price <= 42)][['winery', 'price']].dropna()
+wine_price = new_df[(new_df.price >= 14) & (new_df.price <= 42)][['wine', 'price']].dropna()
+wnry_agg = winery_price.groupby('winery').agg({'price' : 'mean'})
 
 try:
-    fig1 = px.scatter(new_df,y='price' ,color='category')
-    
-    fig2 = px.line(new_df,y='price' ,color='designation')
-    
-    top_wineries = new_df.winery.value_counts()
-    fig3 = px.bar(top_wineries[::-1], orientation='h')
-    
-    tt = new_df.winery.value_counts().dropna()
-    # tt = tt.where(tt.values > 100)
-    fig4 = px.pie(tt, values=tt.values, names=tt.index, title='Presence of wineries in the dataset')
-
-    fig5 = px.bar(df.category.value_counts()[::-1], orientation='h')
-
-    fig6 = go.Figure(data = [data],layout = layout)
+    fig1 = px.line(new_df,y='price' ,color='category', markers=True)
+    fig2 = px.line(new_df,y='price' ,color='designation', markers=True)
+    fig3 = px.line(wnry_agg, markers=True)
+    fig4 = px.bar(top_wineries[::-1], orientation='h')
+    fig5 = px.pie(tt, values=tt.values, names=tt.index, title='Presence of wineries in the dataset')
+    fig6 = px.bar(new_df.category.value_counts()[::-1], orientation='h')
+    fig7 = go.Figure(data = [data],layout = layout)
+    if category:
+        df_spe = df[df.category.isin(category)]
+        fig_spe = px.bar(df_spe.category.value_counts(), orientation='h')
 
     fig1.update_xaxes(showgrid = False)
     fig1.update_yaxes(showgrid = False)
@@ -58,15 +57,27 @@ try:
     fig2.update_yaxes(showgrid = False)
     fig3.update_xaxes(showgrid = False)
     fig3.update_yaxes(showgrid = False)
+    fig4.update_xaxes(showgrid = False)
+    fig4.update_yaxes(showgrid = False)
     fig5.update_xaxes(showgrid = False)
     fig5.update_yaxes(showgrid = False)
+    fig6.update_xaxes(showgrid = False)
+    fig6.update_yaxes(showgrid = False)
+    if fig_spe:
+        fig_spe.update_xaxes(showgrid = False)
+        fig_spe.update_yaxes(showgrid = False)
 
-    st.plotly_chart(fig1)
-    st.plotly_chart(fig2)
-    st.plotly_chart(fig3)
+
+    st.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(fig6)
+    if fig_spe:
+        # print(df_spe.category.value_counts())
+        st.plotly_chart(fig_spe, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig3, use_container_width=True)
     st.plotly_chart(fig4)
     st.plotly_chart(fig5)
-    st.plotly_chart(fig6)
+    st.plotly_chart(fig7, use_container_width=True)
 
 except KeyError as e:
     print(e)
